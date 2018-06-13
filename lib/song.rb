81c74e3803728e145b33d8922f9c5d5dfdbb90a9
Justin Kenney <justinkenney13@gmail.com>
2018-06-13 20:24:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
+
 
 class Song
   attr_accessor :name
