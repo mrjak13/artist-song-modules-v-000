0ec76c8e4b20c638118efc9dce655f2a72a0eb57
Justin Kenney <justinkenney13@gmail.com>
2018-06-13 20:14:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
@@ -26,9 +26,9 @@ class Song
   #   @@songs.clear
   # end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   # def self.count
   #   self.all.count
